bd337cad3b9450332c34b4b4c58efe3af5f1e434
Savio Nascimento <savionascimentodev@gmail.com>
2024-03-18 21:25:48 -0400

    ✨ add new component

--- 09-React-Introducao/rocket-notes/src/pages/SignIn/index.jsx
+++ 09-React-Introducao/rocket-notes/src/pages/SignIn/index.jsx
@@ -2,7 +2,7 @@ import { Input } from "../../components/Input"
 import { FiMail, FiLock } from "react-icons/fi"
 import { Button } from "../../components/Button"
 
-import { Container, Form } from "./styles"
+import { Background, Container, Form } from "./styles"
 export function SignIn() {
   return (
     <Container>
@@ -13,10 +13,12 @@ export function SignIn() {
         <Input placeholder="E-mail" type="text" icon={FiMail} />
         <Input placeholder="Senha" type="password" icon={FiLock} />
 
-        <Button>Entrar</Button>
+        <Button title="Entrar" />
 
         <a href="#">Criar conta</a>
       </Form>
+
+      <Background />
     </Container>
   )
 }
